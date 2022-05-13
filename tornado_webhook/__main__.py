47b3860767b49ea18a786cd6c8229156f3559084
Wentao Han <wentao.han@gmail.com>
2022-05-13 22:08:13 +0800

    Implement MVP.

--- tornado_webhook/__main__.py
+++ tornado_webhook/__main__.py
@@ -1,30 +1,46 @@
 #!/usr/bin/env python3
 
+import yaml
+
 from tornado.escape import json_decode
 from tornado.ioloop import IOLoop
 from tornado.options import define, options, parse_command_line
 from tornado.process import Subprocess
+from tornado.routing import AnyMatches
 from tornado.web import Application, RequestHandler
 
 define('address', default='localhost', help='address to listen at')
 define('port', default=8080, help='port to listen on')
 define('debug', default=False, help='debug mode')
+define('config', default='webhooks.yaml', help='webhook configuration file')
 
 
 class WebhookHandler(RequestHandler):
 
     async def post(self):
-        data = json_decode(self.request.body)
-        time = data.get('time', 1)
-        proc = Subprocess(['sleep', str(time)])
-        code = await proc.wait_for_exit(False)
-        self.write({'code': code})
+        hooks = self.settings['config']['hooks']
+        url = self.request.full_url()
+        # data = json_decode(self.request.body)
+        for hook in hooks:
+            if hook.get('hook') == url:  # TODO: better matching method
+                args = [hook.get('cmd', '/bin/bash')] + hook.get('args', [])
+                proc = Subprocess(args)
+                code = await proc.wait_for_exit(False)
+                self.write({'status': 'success', 'code': code})
+                break
+        else:
+            self.write({'status': 'failure'})
+            self.set_status(400)
 
 
 def make_app():
-    return Application([
-        ('/webhook/', WebhookHandler),
-    ], debug=options.debug)
+    with open(options.config) as config_file:
+        config = yaml.safe_load(config_file)
+    return Application(
+        [(AnyMatches(), WebhookHandler)],
+        config=config,
+        debug=options.debug,
+    )
 
 
 if __name__ == '__main__':
