8061b0272b55cbd57811ade97e095d45809cffc9
Wentao Han <wentao.han@gmail.com>
2022-05-15 11:55:41 +0800

    Check POST arguments.

--- tornado_webhook/__main__.py
+++ tornado_webhook/__main__.py
@@ -16,26 +16,42 @@ define('conf', default='webhooks.yaml', help='path to configuration file')
 define('debug', default=False, help='debug mode')
 
 
+def match_url(repo, url):
+    for key in ['clone_url', 'ssh_url', 'git_url']:
+        if repo.get(key) == url:
+            return True
+    return False
+
+def match_hook(hooks, request):
+    url = request.full_url()
+    data = json_decode(request.body)
+    for hook in hooks:
+        if hook.get('hook') != url:
+            continue
+        if not match_url(data.get('repository'), hook.get('repo')):
+            continue
+        # TODO: Check signature
+        return hook
+    return None  # Hook not matched
+
+
 class WebhookHandler(RequestHandler):
 
     async def post(self):
         hooks = self.settings['conf']['hooks']
-        url = self.request.full_url()
-        data = json_decode(self.request.body)
-        for hook in hooks:
-            if hook.get('hook') == url:  # TODO: better matching method
-                app_log.info(f'Webhook {url} matched')
-                path = hook.get('path', '.')
-                action = hook.get('action', 'true')
-                app_log.info(f'Executing action "{action}"')
-                proc = Subprocess(['/bin/bash', '-c', action], cwd=path)
-                def proc_exit_callback(code):
-                    app_log.info(f'Action "{action}" done with code {code}')
-                proc.set_exit_callback(proc_exit_callback)
-                self.write({'status': 'executing'})
-                break
+        hook = match_hook(hooks, self.request)
+        if hook:
+            app_log.info(f'Webhook {hook["hook"]} for repo {hook["repo"]} matched')
+            path = hook.get('path', '.')
+            action = hook.get('action', 'true')
+            app_log.info(f'Executing action "{action}"')
+            proc = Subprocess(['/bin/bash', '-c', action], cwd=path)
+            def proc_exit_callback(code):
+                app_log.info(f'Action "{action}" done with code {code}')
+            proc.set_exit_callback(proc_exit_callback)
+            self.write({'status': 'executing'})
         else:
-            app_log.warning(f'Webhook {url} not configured')
+            app_log.warning(f'Webhook {self.request.full_url()} not configured')
             self.write({'status': 'failure'})
             self.set_status(400)
 
